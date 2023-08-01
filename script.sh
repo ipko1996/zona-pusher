787fff5f6164ebd8e57c0fd69553324dfc1ab98a
ipko1996 <ipko1996@gmail.com>
2023-08-01 21:23:57 +0200

    (fix): fix bash script parsing

--- script.sh
+++ script.sh
@@ -25,17 +25,22 @@ json_data=$(get_json_from_url "$source_url")
 if [ -n "$json_data" ]; then
     echo "Fetched JSON successfully."
 
-    day_of_week_number=$(get_day_of_week_number)
+    day_of_week_number=$(get_day_of_week_number)-1
     day_of_week=${week_days[$day_of_week_number]}
     random_food_emoji=${food_emojis[$RANDOM % ${#food_emojis[@]}]}
     current_date=$(date +"%m.%d")
 
     text="($current_date) $day_of_week $random_food_emoji\n\n"
-    for item in $(echo "$json_data" | jq -c ".[\"$day_of_week\"][]"); do
-        food=$(echo "$item" | jq -r ".food")
-        price=$(echo "$item" | jq -r ".price")
-        text+="$(echo -e "$food - $price\n\n")"
-    done
+    filtered_data=$(echo "$json_data" | jq -cj ".[\"$day_of_week\"][]")
+    foods_prices=$(echo "$json_data" | jq --arg day "$day_of_week" -r '.[$day]')
+
+    output=""
+    while IFS= read -r line; do
+        output+="$line\n\n"
+    done <<< "$(echo "$foods_prices" | jq -r '.[] | .food + "  -  " + (.price|tostring)')"
+
+    text+=$output
+    echo -e $text
 
     json_data="{\"text\": \"$text\"}"
 
